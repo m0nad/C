d7234f4935183820d13fac8f0ac4cda2e1657848
Victor Ramos Mello <victornrm@gmail.com>
2011-09-06 16:10:28 -0300

    DNS Discovery tool

--- dns-discovery/dns-discovery.c
+++ dns-discovery/dns-discovery.c
@@ -125,7 +125,7 @@ dns_discovery_thread (void * args)
 int
 main (int argc, char ** argv) 
 {
-  int nthreads = 1, i;
+  int nthreads = 1, i, err;
   pthread_t * threads;
   struct dd_threads_args dns_discovery_args;
 
@@ -140,7 +140,9 @@ main (int argc, char ** argv)
   dns_discovery_args.file = ck_fopen (argv[2], "r");
   dns_discovery_args.domain = argv[1];
   for (i = 0; i < nthreads; i++) {
-    pthread_create (&threads[i], NULL, dns_discovery_thread, (void *)&dns_discovery_args);
+    err = pthread_create (&threads[i], NULL, dns_discovery_thread, (void *)&dns_discovery_args);
+    if (err != 0)
+      erro ("pthread ");
   }
   for (i = 0; i < nthreads; i++) {
     pthread_join (threads[i], NULL);
