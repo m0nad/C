81c29dedd85fb3be9466cfcd45a743e4eaece991
Victor Ramos Mello <victornrm@gmail.com>
2011-09-19 22:48:14 -0300

    DNS-Discovery tool

--- dns-discovery/dns-discovery.c
+++ dns-discovery/dns-discovery.c
@@ -38,6 +38,7 @@ IPv6 address: 2001:4860:8006::67
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -47,12 +48,18 @@ IPv6 address: 2001:4860:8006::67
 
 #define TAM 256
 #define MAX 512
-#define SAY printf
+#define SAY(args...) \
+  printf (args);\
+  if (dd_args.report) \
+    fprintf (dd_args.report, args);
 
-struct dd_threads_args {
-  FILE * file;
+struct dns_discovery_args {
+  FILE * report;
   char * domain;
+  int nthreads;
 };
+struct dns_discovery_args dd_args;
+pthread_mutex_t mutexsum;
 
 void
 error (const char * msg)
@@ -106,8 +113,54 @@ banner ()
 int
 usage ()
 {
-  SAY (" usage\n ./dns-discovery domain wordlist.wl [threads]\n\n");
+  SAY ("usage: ./dns-discovery domain [options]\n");
+  SAY ("options:\n");
+  SAY ("\t-w wordlist file (default : wordlist.wl)\n");
+  SAY ("\t-r report file (default : dd_domain)\n");
+  SAY ("\t-t threads (default : 1)\n");
   exit (1);
+}
+FILE *
+parse_args (int argc, char ** argv)
+{
+  FILE * wordlist = NULL;
+  char c; 
+  if (argc < 2) 
+    usage ();
+  dd_args.domain = argv[1];
+  dd_args.nthreads = 1;
+  SAY ("DOMAIN : %s\n", dd_args.domain);
+  argc--;
+  argv++;
+  opterr = 0;
+  while ((c = getopt (argc, argv, "r:w:t:")) != -1)
+    switch (c) {
+      case 'w':
+	SAY ("WORDLIST: %s\n", optarg); 
+	wordlist = ck_fopen (optarg, "r");
+        break;
+      case 't':
+        SAY ("THREADS: %s\n", optarg);
+        dd_args.nthreads = atoi (optarg);
+	break;
+      case 'r':
+        SAY ("REPORT: %s\n", optarg);
+        dd_args.report = ck_fopen (optarg, "a");
+        break;
+      case '?':
+        if (optopt == 'r' || optopt == 'w' || optopt == 't') {
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+	  exit (1);
+        }
+      default:
+        usage ();
+    }
+  if (!wordlist) {
+    wordlist = ck_fopen ("wordlist.wl", "r"); 
+    SAY ("WORDLIST: wordlist.wl\n");
+  }
+  SAY("\n");
+  return wordlist;
 }
 
 void
@@ -124,6 +177,7 @@ resolve_lookup (const char * hostname)
   hints.ai_flags |= AI_CANONNAME;
 
   if (getaddrinfo (hostname, NULL, &hints, &res) == 0) {
+    pthread_mutex_lock (&mutexsum);
     SAY ("%s\n", hostname);
     for (ori_res = res; res; res = res->ai_next) { 
       switch (res->ai_family) {
@@ -140,6 +194,7 @@ resolve_lookup (const char * hostname)
       SAY ("IPv%d address: %s\n", ipv, addr_str);
     }
     SAY("\n");
+    pthread_mutex_unlock (&mutexsum);
     freeaddrinfo (ori_res);
   }
 }
@@ -160,37 +215,36 @@ dns_discovery (FILE * file, const char * domain)
 void *
 dns_discovery_thread (void * args)
 {
-  struct dd_threads_args * dns_discovery_args = (struct dd_threads_args *) args;
-  dns_discovery (dns_discovery_args->file, dns_discovery_args->domain);
+  FILE * wordlist = (FILE *) args;
+  dns_discovery (wordlist, dd_args.domain);
+  //pthread_exit ((void *) 0);
   return NULL;	
 }
 
 int
 main (int argc, char ** argv) 
 {
-  int nthreads = 1, i;
+  int i;
   pthread_t * threads;
-  struct dd_threads_args dns_discovery_args;
+  FILE * wordlist;
 
   banner();
-  if (argc < 3 || argc > 4) 
-    usage();
-  else if (argc == 4)
-    nthreads = atoi (argv[3]);
-
-  threads = (pthread_t *) ck_malloc (nthreads * sizeof (pthread_t)); 
  
-  dns_discovery_args.file = ck_fopen (argv[2], "r");
-  dns_discovery_args.domain = argv[1];
-  for (i = 0; i < nthreads; i++) {
-    if (pthread_create (&threads[i], NULL, dns_discovery_thread, (void *)&dns_discovery_args) != 0)
+  wordlist = parse_args (argc, argv);
+  threads = (pthread_t *) ck_malloc (dd_args.nthreads * sizeof (pthread_t)); 
+ 
+ for (i = 0; i < dd_args.nthreads; i++) {
+    if (pthread_create (&threads[i], NULL, dns_discovery_thread, (void *)wordlist) != 0)
       error ("pthread ");
   }
-  for (i = 0; i < nthreads; i++) {
+  for (i = 0; i < dd_args.nthreads; i++) {
     pthread_join (threads[i], NULL);
   }
 
+  if (dd_args.report)
+    fclose (dd_args.report);
+
   free (threads);
-  fclose (dns_discovery_args.file);
+  fclose (wordlist);
   return 0;
 }
