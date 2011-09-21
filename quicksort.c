52f3129670a1e3d1259cdb712cbaaee3c69c851c
Victor Ramos Mello <victornrm@gmail.com>
2011-09-20 22:26:34 -0300

    implementation of quicksort

---
+++ quicksort.c
@@ -0,0 +1,45 @@
+/*
+implementation of quicksort
+m0nad /at/ email.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5 
+
+void
+quicksort (int * v, int inicio, int fim)
+{
+  int i = inicio, j = fim, aux, meio, x;
+  meio = (inicio + fim) / 2;
+  x = v[meio];
+  while (i <= j) {
+    if (v[i] < x) i++;
+    else if (v[j] > x) j--;
+    else {
+      aux = v[i];
+      v[i++] = v[j];
+      v[j--] = aux;
+    }
+  }
+  if (inicio < j)
+    quicksort (v, inicio, j);
+  if (fim > i)
+    quicksort (v, i , fim);
+}
+
+int
+main ()
+{
+  int * lista, i;
+  int inicio = 0, fim = MAX-1;
+  
+  lista = (int *) malloc (MAX * sizeof (int));
+  for(i = 0; i < MAX; i++)
+    scanf("%d", &lista[i]);
+  quicksort(lista , inicio, fim);
+  for(i = 0; i < MAX; i++)
+    printf("%d ", lista[i]);
+  puts("");
+  return 0;
+}
